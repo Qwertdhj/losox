f60dee779cba68b173acbaaafddc7bb72e2e18cc
Qwertdhj <101670983+Qwertdhj@users.noreply.github.com>
2022-03-15 23:27:02 +0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,66 @@
+
+const sliderMain = new Swiper('.slider_main', {
+	freeMode: true,
+	centeredSlides: true,
+	mousewheel: true,
+	parallax: true,
+	breakpoints: {
+		0: {
+			slidesPerView: 2.5,
+			spaceBetween: 20
+		},
+		680: {
+			slidesPerView: 3.5,
+			spaceBetween: 60
+		}
+	}
+})
+const sliderBg = new Swiper('.slider_bg', {
+	centeredSlides: true,
+	parallax: true,
+	spaceBetween: 60,
+	slidesPerView: 3.5
+})
+sliderMain.controller.control = sliderBg
+
+document.querySelectorAll('.slider__item').forEach(item => {
+	item.addEventListener('click', event => {
+		item.classList.toggle('opened')
+	})
+})
+
+let desc = document.querySelector('.description')
+sliderMain.on('slideChange', e => {
+	sliderMain.activeIndex > 0 ? desc.classList.add('hidden') : desc.classList.remove('hidden')
+})
+const overlay1 = document.querySelector('.overlay-1');
+const overlay2 = document.querySelector('.overlay-2');
+const search = document.querySelector('.search');
+const input = document.querySelector('.input');
+overlay1.addEventListener('click', () => {
+  search.classList.toggle('active');
+  if (search.classList.contains('active')) {
+    setTimeout(() => {
+      input.focus();
+    }, 200)
+  }
+})
+search.addEventListener('click', () => {
+  if (search.classList.contains('active')) {
+    setTimeout(() => {
+      input.focus();
+    }, 200)
+  }
+})
+overlay2.addEventListener('click', (e) => {
+  input.value = '';
+  input.focus();
+  search.classList.remove('searching')
+})
+document.body.addEventListener('click', (e) => {
+  if (!search.contains(e.target) && input.value.length === 0) {
+    search.classList.remove('active');
+    search.classList.remove('searching');
+    input.value = '';
+  }
+})
